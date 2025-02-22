2fa1f19ea05b80ed44d1940e40b7a4629d04ffa4
Amit Mukhiya <amitmukhiya@Amits-MacBook-Air.local>
2025-02-23 01:10:34 +0530

    changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,21 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    rules: {
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",
-      "react/jsx-props-no-spreading": "off",
-      "react/jsx-filename-extension": [
-        "error",
-        { extensions: [".tsx", ".jsx"] },
-      ],
-    },
-  },
+    "parser": "@typescript-eslint/parser",
+    "plugins": [
+      "@typescript-eslint",
+    ],
+    "extends": [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+    ],
+    "rules": {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error"]
+    }
+  }
 ];
 
 export default eslintConfig;
